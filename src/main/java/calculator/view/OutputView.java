545ec1a692e948b7b70eea2a6fbb1569bf38d20f
jisooou <jisoou4@gmail.com>
2025-10-18 00:21:31 +0900

    feat(OutputView): 출력 형식 추가

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String OUTPUT_PROMT = "결과 : ";
+
+    public void printResult(String result){
+        System.out.println(OUTPUT_PROMT + result);
+    }
+}

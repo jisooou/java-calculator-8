f2de94422f611efe1a45599fa212a6ce62fbd155
jisooou <jisoou4@gmail.com>
2025-10-17 19:45:46 +0900

    feat(Calculator): 숫자 합 계산

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,4 +1,15 @@
 package calculator.model;
 
 public class Calculator {
+    //SplitHandler에서 return한 값을 받아서 합한다.
+    public int add(String input){
+        String[] nums = SplitHandler.split(input);
+        int sum = 0;
+        for(String num : nums){
+            int n = Integer.parseInt(num);
+            Validator.validate(n);
+            sum += n;
+        }
+        return sum; 
+    }
 }
--- src/main/java/calculator/model/Validator.java
+++ src/main/java/calculator/model/Validator.java
@@ -1,4 +1,7 @@
 package calculator.model;
 
 public class Validator {
+    public static void validate(int n){
+
+    }
 }

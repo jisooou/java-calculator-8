86366f27ec8881d9a3432b8500d589b20ea12b44
jisooou <jisoou4@gmail.com>
2025-10-18 16:39:37 +0900

    feat(Application): Controller 호출하여 main 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//        1. CalculatorController 호출
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Calculator calculator = new Calculator();
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView, calculator);
+        calculatorController.runMain();
     }
 }

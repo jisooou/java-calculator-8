8a18b8d29672b207e7b30ef484811778de98a9a5
jisooou <jisoou4@gmail.com>
2025-10-18 16:46:24 +0900

    fix(OutputView): 출력 타입 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,7 +3,7 @@ package calculator.view;
 public class OutputView {
     private static final String OUTPUT_PROMT = "결과 : ";
 
-    public void printResult(String result){
+    public void printResult(Integer result){
         System.out.println(OUTPUT_PROMT + result);
     }
 }

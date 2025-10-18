51fabe161a2be78fbd4dcf73767bc9a2e4b4c636
jisooou <jisoou4@gmail.com>
2025-10-18 16:48:02 +0900

    feat(Calculator): IllegalArgumentException 경우 추가

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -3,10 +3,21 @@ package calculator.model;
 public class Calculator {
     //SplitHandler에서 return한 값을 받아서 합한다.
     public int add(String input){
+        //input 문자열이 null이거나 비어있는 경우(type: string)
+        if(input == null || input.isEmpty()){
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+
         String[] nums = SplitHandler.split(input);
         int sum = 0;
         for(String num : nums){
-            int n = Integer.parseInt(num);
+            int n;
+            try{
+                n = Integer.parseInt(num);
+            } catch (NumberFormatException e) {
+                //num에 숫자가 아닌 문자가 포함되어 있는 경우
+                throw new IllegalArgumentException("입력값에 숫자가 아닌 문자가 포함되어 있습니다.");
+            }
             Validator.validate(n);
             sum += n;
         }

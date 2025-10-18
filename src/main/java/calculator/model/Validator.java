069c2bf783dbf27df44d89e7f28a8e9685d84174
jisooou <jisoou4@gmail.com>
2025-10-18 16:50:48 +0900

    fix(Validator): 불필요한 코드 삭제

--- src/main/java/calculator/model/Validator.java
+++ src/main/java/calculator/model/Validator.java
@@ -1,19 +1,10 @@
 package calculator.model;
 
 public class Validator {
-
-    // 입력값에 대한 validator도 이 파일 안에서 만든다.
-    private final String inputString;
-
-    public Validator(String inputString) {
-        this.inputString = inputString;
-    }
-
     public static void validate(int input){
-        // 음수인 경우
+        // 음수인 경우(type: int)
         if (input < 0){
-            throw new IllegalArgumentException("음수를 입력할 수 없습니다.");
+            throw new IllegalArgumentException("입력값은 음수일 수 없습니다.");
         }
-
     }
 }

2b852260af5331972d3d21491b2da56d39020c14
jisooou <jisoou4@gmail.com>
2025-10-18 16:41:53 +0900

    feat(CalculatorController): InputView, OutputView 호출과 Calculator 호출하여 계산기 기능 수행

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,27 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.model.Validator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+
+    public CalculatorController(InputView inputView, OutputView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculator = calculator;
+    }
+
+//    1. InputView에서 입력받은 문자열 전달 받는다.
+//    2. Calculator를 호출한다. (Calculator 내에 SplitHandler, Validator)
+//    3. OutputView로 최종 결과를 전달한다.
+    public void runMain(){
+        String input = inputView.readInput();
+        int result = calculator.add(input);
+        outputView.printResult(result);
+    }
 }

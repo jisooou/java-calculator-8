0de3839eec3cc9e97acc237aaa0d66a4a7c14cd4
jisooou <jisoou4@gmail.com>
2025-10-18 16:45:12 +0900

    fix(InputView): 리턴값 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,9 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
 
-    public Validator validateInput(){
+    public String readInput(){
         System.out.println(INPUT_PROMPT);
-        String inputString = Console.readLine();
-        return new Validator(inputString);
+        return Console.readLine();
     }
 }
